0e50bb5bbaee487cf515c9a8c4f82835d66a0ffd
KumarGN69 <kumargn1969@hotmail.com>
2024-07-14 20:44:29 +0000

    deleted readpdf.py

--- readpdf.py
+++
@@ -1,9 +0,0 @@
-from langchain_community.document_loaders import PyPDFLoader
-
-file_path = "./MSOW.pdf"
-loader = PyPDFLoader(file_path)
-pages = loader.load_and_split()
-
-print(type(pages))
-for page in pages:
-    print(page)

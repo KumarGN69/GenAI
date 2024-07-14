901bf36cf2fbbc849638107dde6e5aff7952f380
KumarGN69 <kumargn1969@hotmail.com>
2024-07-14 20:39:41 +0000

    Vector DB similarity search

--- read_files.py
+++ read_files.py
@@ -1,9 +1,18 @@
 from langchain_community.document_loaders import PyPDFLoader
-file_path = "./MSOW.pdf"
+from langchain_chroma import Chroma 
+from langchain_community.embeddings import OllamaEmbeddings
 
+#Load the pdf files and split them into chunks
+file_path = "./MSOW.pdf"
 loader = PyPDFLoader(file_path)
 
 pages = loader.load_and_split()
 # print(pages)
-for page in pages:
-    print(page)
+# for page in pages:
+#     print(page)
+
+# Setup Vector Db Chroma
+vector_store = Chroma.from_documents(documents=pages, embedding=OllamaEmbeddings(model="llama3"))
+answers = vector_store.similarity_search("WHo is the project manager from Meta")
+for answer in answers:
+    print(answer)

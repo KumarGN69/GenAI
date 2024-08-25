93dec926b03e139799808828aac6bca1ca7c7b28
KumarGN69 <kumargn1969@hotmail.com>
2024-08-25 17:19:09 +0000

    changes to gitpod.yml and streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit as st
 x = st.slider("Select a value")
 st.write(x, "squared is", x * x)
+add_selectbox = st.sidebar.selectbox(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone")
+)
